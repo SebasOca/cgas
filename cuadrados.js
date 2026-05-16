25d3e4d873f39bbe715bbdc9c859ec18c9414bdc
SebasOca <129012466+SebasOca@users.noreply.github.com>
2026-05-16 00:19:38 -0300

    Add files via upload

--- cuadrados.js
+++ cuadrados.js
@@ -1,28 +1,31 @@
 function cuadrados() {
+  rotate(0.0046);
   push();
-  for (let j = 0; j < 100; j = j+10) {
+  for (let j = 0; j < 180; j = j+10) {
     lineaCuadrado(posXCuadrados+j, posYCuadrados, colorLineaArriba1, colorLineaMedio1, colorLineaAbajo1);
-    lineaCuadrado(posXCuadrados+1+j, posYCuadrados, colorLineaArriba2, colorLineaMedio2, colorLineaAbajo2);
-    lineaCuadrado(posXCuadrados+2+j, posYCuadrados, colorLineaArriba3, colorLineaMedio3, colorLineaAbajo3);
-    lineaCuadrado(posXCuadrados+3+j, posYCuadrados, colorLineaArriba4, colorLineaMedio4, colorLineaAbajo4);
-    lineaCuadrado(posXCuadrados+4+j, posYCuadrados, colorLineaArriba5, colorLineaMedio5, colorLineaAbajo5);
-    lineaCuadrado(posXCuadrados+5+j, posYCuadrados, colorLineaArriba6, colorLineaMedio6, colorLineaAbajo6);
-    lineaCuadrado(posXCuadrados+6+j, posYCuadrados, colorLineaArriba7, colorLineaMedio7, colorLineaAbajo7);
-    lineaCuadrado(posXCuadrados+7+j, posYCuadrados, colorLineaArriba8, colorLineaMedio8, colorLineaAbajo8);
-    lineaCuadrado(posXCuadrados+8+j, posYCuadrados, colorLineaArriba9, colorLineaMedio9, colorLineaAbajo9);
-    lineaCuadrado(posXCuadrados+9+j, posYCuadrados, colorLineaArriba10, colorLineaMedio10, colorLineaAbajo10);
-  }
+    lineaCuadrado(posXCuadrados+4+j, posYCuadrados, colorLineaArriba2, colorLineaMedio2, colorLineaAbajo2);
+    lineaCuadrado(posXCuadrados+8+j, posYCuadrados, colorLineaArriba3, colorLineaMedio3, colorLineaAbajo3);
+    lineaCuadrado(posXCuadrados+12+j, posYCuadrados, colorLineaArriba4, colorLineaMedio4, colorLineaAbajo4);
+    lineaCuadrado(posXCuadrados+16+j, posYCuadrados, colorLineaArriba5, colorLineaMedio5, colorLineaAbajo5);
+    lineaCuadrado(posXCuadrados+20+j, posYCuadrados, colorLineaArriba6, colorLineaMedio6, colorLineaAbajo6);
+    lineaCuadrado(posXCuadrados+24+j, posYCuadrados, colorLineaArriba7, colorLineaMedio7, colorLineaAbajo7);
+    lineaCuadrado(posXCuadrados+28+j, posYCuadrados, colorLineaArriba8, colorLineaMedio8, colorLineaAbajo8);
+    lineaCuadrado(posXCuadrados+32+j, posYCuadrados, colorLineaArriba9, colorLineaMedio9, colorLineaAbajo9);
+    lineaCuadrado(posXCuadrados+36+j, posYCuadrados, colorLineaArriba10, colorLineaMedio10, colorLineaAbajo10);
+}
   pop();
 }
 
 function lineaCuadrado(posX, posY, colorArriba, colorMedio, colorAbajo) {
   for (let x = 0; x < 1; x++) {
     for (let i = 0; i < 10; i++) {
-      let intermedio = map(i, -50, 250, 0, 1);
+      let intermedio = map(i, 0, 255, 0, 1);
+
+      strokeWeight(4);
 
       let degrade = lerpColor(colorArriba, colorMedio, intermedio);
       stroke(degrade);
-      line(posX+x, posY+i, posX+0.5+x, posY+100+i);
+      line(posX+x, posY+i, posX+1.5+x, posY+200+i);
     }
   }
 }
--- tp1.js
+++ tp1.js
@@ -1,6 +1,7 @@
-let colorLineaArriba1, colorLineaArriba2, colorLineaArriba3, colorLineaArriba4, colorLineaArriba5, colorLineaArriba6, colorLineaArriba7, colorLineaArriba8, colorLineaArriba9, colorLineaArriba10, colorLineaArriba11, colorLineaArriba12;
-let colorLineaMedio1, colorLineaMedio2, colorLineaMedio3, colorLineaMedio4, colorLineaMedio5, colorLineaMedio6, colorLineaMedio7, colorLineaMedio8, colorLineaMedio9, colorLineaMedio10, colorLineaMedio11, colorLineaMedio12;
-let colorLineaAbajo1, colorLineaAbajo2, colorLineaAbajo3, colorLineaAbajo4, colorLineaAbajo5, colorLineaAbajo6, colorLineaAbajo7, colorLineaAbajo8, colorLineaAbajo9, colorLineaAbajo10, colorLineaAbajo11, colorLineaAbajo12;
+let obra;
+let colorLineaArriba0, colorLineaArriba1, colorLineaArriba2, colorLineaArriba3, colorLineaArriba4, colorLineaArriba5, colorLineaArriba6, colorLineaArriba7, colorLineaArriba8, colorLineaArriba9, colorLineaArriba10, colorLineaArriba11;
+let colorLineaMedio0, colorLineaMedio1, colorLineaMedio2, colorLineaMedio3, colorLineaMedio4, colorLineaMedio5, colorLineaMedio6, colorLineaMedio7, colorLineaMedio8, colorLineaMedio9, colorLineaMedio10, colorLineaMedio11;
+let colorLineaAbajo0, colorLineaAbajo1, colorLineaAbajo2, colorLineaAbajo3, colorLineaAbajo4, colorLineaAbajo5, colorLineaAbajo6, colorLineaAbajo7, colorLineaAbajo8, colorLineaAbajo9, colorLineaAbajo10, colorLineaAbajo11;
 let colorAz1, colorAz2, colorAz3, colorAm1, colorAm2, colorAm3, colorNar1, colorNar2, colorNar3;
 
 let posXFondo = 0;
@@ -9,48 +10,48 @@ let posYCuadrados = 83;
 
 
 function preload() {
-  colorLineaArriba1 = color (113, 133, 103);
-  colorLineaArriba2 = color (36, 76, 101);
-  colorLineaArriba3 = color (21, 39, 38);
-  colorLineaArriba4 = color (121, 151, 115);
-  colorLineaArriba5 = color (37, 68, 89);
-  colorLineaArriba6 = color (5, 24, 38);
-  colorLineaArriba7 = color (115, 159, 151);
-  colorLineaArriba8 = color (40, 79, 86);
-  colorLineaArriba9 = color (12, 29, 20);
-  colorLineaArriba10 = color (110, 139, 145);
-  colorLineaArriba11 = color (45, 90, 108);
-  colorLineaArriba12 = color (16, 32, 25);
+  colorLineaArriba0 = color (16, 32, 25, 100);
+  colorLineaArriba1 = color (113, 133, 103, 100);
+  colorLineaArriba2 = color (36, 76, 101, 100);
+  colorLineaArriba3 = color (21, 39, 38, 100);
+  colorLineaArriba4 = color (121, 151, 115, 100);
+  colorLineaArriba5 = color (37, 68, 89, 100);
+  colorLineaArriba6 = color (5, 24, 38, 100);
+  colorLineaArriba7 = color (115, 159, 151, 100);
+  colorLineaArriba8 = color (40, 79, 86, 100);
+  colorLineaArriba9 = color (12, 29, 20, 100);
+  colorLineaArriba10 = color (110, 139, 145, 100);
+  colorLineaArriba11 = color (45, 90, 108, 100);
 
-  colorLineaMedio1 = color (25, 35, 47);
-  colorLineaMedio2 = color (74, 59, 36);
-  colorLineaMedio3 = color (25, 28, 27);
-  colorLineaMedio4 = color (91, 87, 83);
-  colorLineaMedio5 = color (100, 79, 55);
-  colorLineaMedio6 = color (19, 30, 29);
-  colorLineaMedio7 = color (81, 78, 89);
-  colorLineaMedio8 = color (127, 110, 89);
-  colorLineaMedio9 = color (29, 34, 43);
-  colorLineaMedio10 = color (87, 78, 83);
-  colorLineaMedio11 = color (116, 97, 86);
-  colorLineaMedio12 = color (23, 37, 47);
+  colorLineaMedio0 = color (23, 37, 47, 50);
+  colorLineaMedio1 = color (25, 35, 47, 50);
+  colorLineaMedio2 = color (74, 59, 36, 50);
+  colorLineaMedio3 = color (25, 28, 27, 50);
+  colorLineaMedio4 = color (91, 87, 83, 50);
+  colorLineaMedio5 = color (100, 79, 55, 50);
+  colorLineaMedio6 = color (19, 30, 29, 50);
+  colorLineaMedio7 = color (81, 78, 89, 50);
+  colorLineaMedio8 = color (127, 110, 89, 50);
+  colorLineaMedio9 = color (29, 34, 43, 50);
+  colorLineaMedio10 = color (87, 78, 83, 50);
+  colorLineaMedio11 = color (116, 97, 86, 50);
 
-  colorLineaAbajo1 = color (40, 39, 21);
-  colorLineaAbajo2 = color (118, 144, 92);
-  colorLineaAbajo3 = color (45, 82, 79);
-  colorLineaAbajo4 = color (48, 46, 29);
-  colorLineaAbajo5 = color (129, 153, 112);
-  colorLineaAbajo6 = color (21, 75, 68);
-  colorLineaAbajo7 = color (43, 54, 20);
-  colorLineaAbajo8 = color (121, 149, 118);
-  colorLineaAbajo9 = color (20, 52, 60);
-  colorLineaAbajo10 = color (34, 34, 12);
-  colorLineaAbajo11 = color (124, 144, 98);
-  colorLineaAbajo12 = color (30, 64, 83);
+  colorLineaAbajo0 = color (30, 64, 83, 100);
+  colorLineaAbajo1 = color (40, 39, 21, 100);
+  colorLineaAbajo2 = color (118, 144, 92, 100);
+  colorLineaAbajo3 = color (45, 82, 79, 100);
+  colorLineaAbajo4 = color (48, 46, 29, 100);
+  colorLineaAbajo5 = color (129, 153, 112, 100);
+  colorLineaAbajo6 = color (21, 75, 68, 100);
+  colorLineaAbajo7 = color (43, 54, 20, 100);
+  colorLineaAbajo8 = color (121, 149, 118, 100);
+  colorLineaAbajo9 = color (20, 52, 60, 100);
+  colorLineaAbajo10 = color (34, 34, 12, 100);
+  colorLineaAbajo11 = color (124, 144, 98, 100);
 }
 
 function setup() {
-  createCanvas (400, 400);
+  createCanvas (800, 800);
   background('#222222');
 }
 
@@ -66,13 +67,13 @@ function draw() {
 function movimiento () {
   if (keyIsPressed === true) {
     if (keyCode === LEFT_ARROW) {
-      posXCuadrados= posXCuadrados-2;
+      posXCuadrados= posXCuadrados-4;
     } else if (keyCode === RIGHT_ARROW) {
-      posXCuadrados= posXCuadrados+2;
+      posXCuadrados= posXCuadrados+4;
     } else if (keyCode === UP_ARROW) {
-      posYCuadrados= posYCuadrados-2;
+      posYCuadrados= posYCuadrados-4;
     } else if (keyCode === DOWN_ARROW) {
-      posYCuadrados= posYCuadrados+2;
+      posYCuadrados= posYCuadrados+4;
     }
   }
 }
